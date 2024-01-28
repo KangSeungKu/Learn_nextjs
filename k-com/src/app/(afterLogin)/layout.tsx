d5402afa3f3960b013fecfa01423360e7a7b79ce
SeungKu_Kang <kangsg26@naver.com>
2024-01-29 01:40:25 +0900

    * [ZeroCho] '섹션1. 기획자와 디자이너가 기획서를 던져주었다' 진행
    - Next.js의 폴더구성이 곧 페이지구성(router)가 됨.
    - 회원가입, 로그인 페이지 추가
    - modal관련 개념

---
+++ k-com/src/app/(afterLogin)/layout.tsx
@@ -0,0 +1,18 @@
+import {ReactNode} from "react";
+import style from "./layout.module.css"
+
+export default function AfterLoginLayout({ children }: { children: ReactNode }) {
+  return (
+    <div className={style.container}>
+        <header className={style.leftSectionWrapper}>
+            <section className={style.leftSection}></section>
+        </header>
+        <div className={style.rightSectionWrapper}>
+            <div className={style.rightSectionInner}>
+
+            </div>
+        </div>
+      {children}
+    </div>
+  )
+}
---
+++ k-com/src/app/(beforeLogin)/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+import Main from "./_component/Main";
+
+export default function Home() {
+  return (
+    <Main />
+  );
+}
