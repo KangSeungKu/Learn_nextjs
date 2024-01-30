b45cb88cf3816b39d52c54646ccfd657aeaf75b9
SeungKu_Kang <kangsg26@naver.com>
2024-01-30 21:30:33 +0900

    feat(ZeroCho): '섹션2. 본격 클론 시작!'파트 완료

--- k-com/src/app/(afterLogin)/explore/page.tsx
+++ k-com/src/app/(afterLogin)/explore/page.tsx
@@ -1,9 +1,26 @@
-import styles from './page.module.css';
+import SearchForm from '../_component/SearchForm';
+import Trend from '../_component/Trend';
+import style from './explore.module.css';
 
 export default function Page() {
-    return (
-      <main className={styles.main}>
-        탐색
-      </main>
-    )
-  }
+  return (
+    <main className={style.main}>
+      <div className={style.formZone}>
+        <SearchForm />
+      </div>
+      <div className={style.trend}>
+        <h3>나를 위한 트렌드</h3>
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+      </div>
+    </main>
+  )
+}
