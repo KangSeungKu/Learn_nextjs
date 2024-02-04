bea9bd01bb4569430569c8924d5f8ba780336a3a
SeungKu_Kang <kangsg26@naver.com>
2024-02-04 23:42:18 +0900

    feat(ZeroCho): 'react-query SSR 설정하기' 진행
    모듈추가
    : 'react-query' 추가 및 기본설정
    : RQProvider를 통해 개발자 도구추가

---
+++ k-com/src/app/(afterLogin)/_component/RQProvider.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import React, {useState} from "react";
+import {QueryClientProvider, QueryClient} from "@tanstack/react-query";
+import {ReactQueryDevtools} from "@tanstack/react-query-devtools";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+function RQProvider({children}: Props) {
+  const [client] = useState(
+    new QueryClient({
+      defaultOptions: {  // react-query 전역 설정
+        queries: {
+          refetchOnWindowFocus: false,
+          retryOnMount: true,
+          refetchOnReconnect: false,
+          retry: false,
+        },
+      },
+    })
+  );
+
+  return (
+    <QueryClientProvider client={client}>
+        {/* Provider의 장점 - reactQuery data를 서로 공유할 수 있게 됨. */}
+      {children}
+      {/* dev모드일 때만 사용하도록 수정 */}
+      <ReactQueryDevtools initialIsOpen={process.env.NEXT_PUBLIC_MODE === 'local' }/>
+    </QueryClientProvider>
+  );
+}
+
+export default RQProvider;
--- k-com/src/app/(afterLogin)/home/page.tsx
+++ k-com/src/app/(afterLogin)/home/page.tsx
@@ -1,26 +1,48 @@
+import { HydrationBoundary, QueryClient, dehydrate } from '@tanstack/react-query'
 import Post from '../_component/Post'
 import PostForm from './_component/PostForm'
 import Tab from './_component/Tab'
 import TabProvider from './_component/TabProvider'
 import styles from './home.module.css'
 
-export default function Home() {
+async function getPostRecommends() {
+  const res = await fetch(`http://localhost:9090/api/postRecommends`, {
+    next: {
+      tags: ['posts', 'recommends'],
+    },
+    cache: 'no-store',
+  });
+
+  if(!res.ok) {
+    throw new Error('Failed to fetch data');
+  }
+
+  return res.json();
+}
+
+export default async function Home() {
+  const queryClient = new QueryClient();
+  await queryClient.prefetchQuery({queryKey: ['posts', 'recommends'], queryFn: getPostRecommends})
+  // 서버에서 온 데이터를 클라이언트에서 형식 그대로 맞춰서 받는 것을 의미
+  const dehydratedState = dehydrate(queryClient);
     return (
       <main className={styles.main}>
-        <TabProvider>
-            <Tab />
-            <PostForm />
-            <Post />
-            <Post />
-            <Post />
-            <Post />
-            <Post />
-            <Post />
-            <Post />
-            <Post />
-            <Post />
-            <Post />
-        </TabProvider>
+        <HydrationBoundary state={dehydratedState}>
+          <TabProvider>
+              <Tab />
+              <PostForm />
+              <Post />
+              <Post />
+              <Post />
+              <Post />
+              <Post />
+              <Post />
+              <Post />
+              <Post />
+              <Post />
+              <Post />
+          </TabProvider>
+        </HydrationBoundary>
       </main>
     )
   }
